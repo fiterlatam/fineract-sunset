0f30a398c66b611f0c9772a718a214ab0a135019
Farooq <farooq@fiter.io>
2025-03-23 14:39:35 +0000

    Merged PR 8557: Deploy to UAT
    Related work items: #1249, #1250, #1251, #1252, #1253, #1254, #1255, #1256, #1257, #1258, #1259, #1260, #1261, #1262, #1263, #1264, #1265, #1266, #1268, #1270, #1271, #1273, #1274, #1275, #1276, #1277, #1280, #1283, #1284, #1285, #1288, #1289, #1290, #1294, #1295, #1296, #1297, #1327, #1333, #1338, #1339, #1340, #1343, #1354, #1367, #1368, #1369, #1388, #1578, #1611, #1619, #1628, #1629, #1631, #1634, #1635, #1637, #1642, #1645, #1651, #1666, #1675, #1678, #1679, #1685, #1686, #1696, #1697, #1700, #1701, #1705, #1706, #1708, #1715

--- fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
+++ fineract-loan/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/Loan.java
@@ -1264,7 +1264,6 @@ public class Loan extends AbstractAuditableWithUTCDateTimeCustom {
         }
         return amount.plus(finalAmount);
     }
-
     @SuppressWarnings("java:S3776")
     private Money calculateInstallmentChargeAmount(final ChargeCalculationType calculationType, final BigDecimal percentage,
             final LoanRepaymentScheduleInstallment installment, Long parentChargeId, LoanCharge loanCharge) {
