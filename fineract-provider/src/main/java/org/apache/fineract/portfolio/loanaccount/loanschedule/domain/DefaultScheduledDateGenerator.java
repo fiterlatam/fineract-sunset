8f38dedb5beeff5c2abcb4670cd95a1c58f43c93
Farooq <farooq@fiter.io>
2025-04-13 20:28:06 +0000

    Merged PR 8722: Deploy to UAT
    Related work items: #1249, #1250, #1251, #1252, #1253, #1254, #1255, #1256, #1257, #1258, #1259, #1260, #1261, #1262, #1263, #1264, #1265, #1266, #1268, #1270, #1271, #1273, #1274, #1275, #1276, #1277, #1280, #1283, #1284, #1285, #1288, #1289, #1290, #1294, #1295, #1296, #1297, #1327, #1333, #1338, #1339, #1340, #1343, #1354, #1367, #1368, #1369, #1388, #1578, #1611, #1715, #1717, #1721, #1726, #1728, #1729, #1730, #1731, #1733, #1734, #1742, #1747, #1772, #1778, #1787

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/domain/DefaultScheduledDateGenerator.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/loanschedule/domain/DefaultScheduledDateGenerator.java
@@ -347,6 +347,8 @@ public class DefaultScheduledDateGenerator implements ScheduledDateGenerator {
             case MONTHS:
                 if (loanCalendar == null) {
                     idealDisbursementDate = firstRepaymentDate.minusMonths(repaidEvery);
+
+                    // If First payment date is set, get idealDisbursementDate from loanApplicationTerms;
                     if (Boolean.FALSE.equals(idealDisbursementDate.isEqual(loanApplicationTerms.getExpectedDisbursementDate()))) {
                         idealDisbursementDate = loanApplicationTerms.getExpectedDisbursementDate();
                     }
