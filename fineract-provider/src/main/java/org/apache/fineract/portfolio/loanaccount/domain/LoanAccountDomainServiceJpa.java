3e4be83c9ba5a6609cb3206c3f231470e98245db
Daniel Stancampiano <danielsta@fiter.io>
2025-07-25 15:56:34 +0000

    Merged PR 9339: feature/EA-388-418 - Transfer Overpayments for Non-Revolving Loans + Update baseline
    feature/EA-388-418 - Transfer Overpayments for Non-Revolving Loans + Update baseline
    
    Related work items: #31634

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanAccountDomainServiceJpa.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/domain/LoanAccountDomainServiceJpa.java
@@ -237,7 +237,7 @@ public class LoanAccountDomainServiceJpa implements LoanAccountDomainService {
             this.noteRepository.save(note);
         }
 
-        if(this.configurationDomainService.executeGenerateGLEntries()) {
+        if (this.configurationDomainService.executeGenerateGLEntries()) {
             postJournalEntries(loan, existingTransactionIds, existingReversedTransactionIds, isAccountTransfer, isLoanToLoanTransfer);
         }
 
